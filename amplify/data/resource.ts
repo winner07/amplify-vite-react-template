816d917e9f4bc6c563f4d21f301f164a008abcf1
Andrii Taradiuk <andriy.taradiuk@tvh.com>
2025-11-30 21:39:06 +0200

    Changed authorization policy

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -11,7 +11,7 @@ const schema = a.schema({
     .model({
       content: a.string(),
     })
-    .authorization((allow) => [allow.publicApiKey()]),
+    .authorization((allow) => [allow.owner()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
